310b227927e41bccca4a8df239fe1e8916edf2e6
Евгений <msk9199625687@yandex.ru>
2022-10-15 23:03:59 +0300

    change routers

--- src/index.js
+++ src/index.js
@@ -1,53 +1,12 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT ?? 3000
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT ?? 3000;
 
-//html
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'yandex-middle.html'))
-})
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'yandex-middle.html'));
+});
 
-app.get('/chats.html', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'chats.html'))
-})
+app.use(express.static(path.join(__dirname, '..', "dist")));
 
-app.get('/reg.html', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'reg.html'))
-})
-
-app.get('/set.html', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'set.html'))
-})
-
-
-//styles
-
-app.get('/yandex-middle.css', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'yandex-middle.css'))
-})
-
-app.get('/yandex-middle.css.map', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'yandex-middle.css.map'))
-})
-
-
-//fonts
-
-app.get('/opensans.woff2', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'opensans.woff2'))
-})
-
-app.get('/opensans.woff', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'opensans.woff'))
-})
-
-app.get('/opensansbold.woff', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'opensansbold.woff'))
-})
-
-app.get('/opensansbold.woff2', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '..', 'dist', 'opensansbold.woff2'))
-})
-
-app.listen(PORT)
+app.listen(PORT);
