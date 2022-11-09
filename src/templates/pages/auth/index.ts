a5d31675dd1739c68edbf8600ec7f610b6e589d8
Евгений <msk9199625687@yandex.ru>
2022-11-09 23:19:25 +0300

    deploy

--- src/templates/pages/auth/index.ts
+++ src/templates/pages/auth/index.ts
@@ -1,8 +1,56 @@
-import template from './index.tmpl';
-import Block from '../../../utils/Block';
+import Inputs from '/src/templates/components/inputs/inputs';
+import Links from '/src/templates/components/links/links';
+import Auth from './auth';
+import renderDom from '../../../utils/renderDom';
+import Controller from '../../../utils/Controller';
 
-export default class Auth extends Block {
-  render() {
-    return this.compile(template);
+const inputs = new Inputs('div', {
+  items: [{
+    title: 'Login',
+    classInput: 'input auth__input auth__input--login',
+    classLabel: 'label auth__label',
+    name: 'login',
+    type: 'text',
+    id: 'auth_login',
+  }, {
+    title: 'Password',
+    classInput: 'input auth__input auth__input--pass',
+    classLabel: 'label auth__label',
+    name: 'password',
+    type: 'password',
+    id: 'auth_pass',
+  }],
+  attr: {
+    class: 'auth__input-wrapper',
+  },
+  events: {
+    focusin: (evt: EventListenerOrEventListenerObject) => Controller.onValidate(evt),
+    focusout: (evt: EventListenerOrEventListenerObject) => Controller.onValidate(evt),
+  },
+});
+
+const links = new Links('div', {
+  items: [{
+    url: './templates/pages/chat/chat.html',
+    title: 'Sign In',
+    class: 'btn btn--sign',
+  }, {
+    url: './templates/pages/reg/reg.html',
+    title: 'Registration',
+    class: 'btn btn__link btn__link--reg',
+  }],
+  attr: {
+    class: 'auth__btn-wrapper',
   }
-}
+});
+
+const form = new Auth('form', {
+  title: 'Authorization',
+  inputs,
+  links,
+  attr: {
+    class: 'form auth',
+  },
+});
+
+renderDom('.app', form);
