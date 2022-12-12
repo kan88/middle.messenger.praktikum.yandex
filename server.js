0c51281d0a98d8dda128e2f56033ba69697d439a
Евгений <msk9199625687@yandex.ru>
2022-12-12 21:55:32 +0300

    server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(`${__dirname}/dist`));
 
-app.use('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/index.html'));
+app.get("/*", (req, res) => {
+  return res.sendFile(path.join(`${__dirname}/dist/index.html`));
 });
 
 
